615191519d86f1915abe8cb13f87fd4e654fe275
Riddhigoyal <72336061+Riddhigoyal@users.noreply.github.com>
2021-02-08 17:23:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+    
+}
+
+function setup(){
+   Canvas=createCanvas(350,300);
+   Canvas.center()
+}
+
+function draw(){
+
+}
+
+function take_snaoshot(){
+   save('student.jpg');
+}
